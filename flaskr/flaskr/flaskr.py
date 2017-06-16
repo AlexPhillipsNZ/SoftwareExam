4c3c2abf2a634ee787715a0bd670db3d42dc4a2f
Alexander Phillips (1000024961) <PHILAS1@student.op.ac.nz>
2017-06-16 14:30:45 +1200

    Step 5: INCOMPLETE

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -21,6 +21,19 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
@@ -34,3 +47,5 @@ def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+		
+		
