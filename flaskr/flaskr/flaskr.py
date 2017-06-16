77e109c28021bc0838e8200d97c26d5a44d680cb
Alexander Phillips (1000024961) <PHILAS1@student.op.ac.nz>
2017-06-16 15:19:45 +1200

    Testing Step

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -8,7 +8,7 @@ app.config.from_object(__name__) # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'H:\2017S1\SoftwareExam\SoftwareExam\flaskr\flaskr\tmpflaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
@@ -20,6 +20,18 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -34,18 +46,6 @@ def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
 
 @app.route('/')
 def show_entries():
@@ -87,8 +87,3 @@ def login():
     return render_template('login.html', error=error)
 
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
